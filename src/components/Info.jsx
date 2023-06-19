f4a662ca1758ef0826ef5c0702c9c9eddc31c601
Joshua <joeokat@gmail.com>
2023-06-19 20:02:23 +0000

    Update Info.jsx

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -9,14 +9,14 @@ export default function Info() {
           Hi there, I am Joshua, founder of Fleet Labs Ghana and JoeOkat Hub.
         </p>
         <p>
-          I am creating free websites and mobile applications for small
+          I am creating affordable websites and mobile applications for small
           businesses from now to December 2023. Don't miss out on this rare
           opportunity.
         </p>
       </div>
       <div className="my--skills">
         <h3>Skills</h3>
-        <p>UI/UX Design, Web Development, Mobile App Design, Programming.</p>
+        <p>Project Management, UI/UX Design, Web Development, Mobile App Design, Tech Consultancy, Social Media Management.</p>
       </div>
     </div>
   );
