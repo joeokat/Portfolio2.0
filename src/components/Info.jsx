fe5623d6c35bd33817693408fc47f53d9b866b16
Joshua <joeokat@gmail.com>
2023-12-21 06:30:14 +0000

    Card component

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,16 +6,23 @@ export default function Info() {
       <div className="about--me">
         <h3>About</h3>
         <p>
-          Hi there! I am Joshua, founder of Fleet Labs Ghana and BYOI.
+          Hello! I am Joshua, founder and CEO of Fleet Labs Ghana and BYOI.
         </p>
         <p>
-          I am building affordable Websites and Mobile Apps for small and medium size businesses from now to July 2024. Don't miss out on this rare opportunity to grow your business.
+          I am building affordable Web Apps for small and medium size businesses from now to July 2024. Don't miss out on this rare opportunity to grow your business.
         </p>
       </div>
-      <div className="my--skills">
-        <h3>Skills</h3>
-        <p>Project Management, Branding, UI/UX Design, Web Design, App Design, Tech Consultancy, Social Media Management.</p>
+
+      <div className="card">
+        <h2>Get real world skills! <br /> Go from Zero to Boosu!</h2>
+        <p className="b--ads">Are you feeling stuck or overwhelmed in your career journey? Don't worry, <span>BYOI</span> got your back! Together, we'll work to level up your skills, increase your earning potential, and build a brighter future.</p>
+        <a href=""><button className="join--button">Join us today</button></a>
       </div>
+
+      {/* <div className="my--skills">
+         <h3>Skills</h3>
+         <p>Project Management, Branding, UI/UX Design, Web Design, App Design, Tech Consultancy, Social Media Management.</p> 
+      </div>*/}
     </div>
   );
 }
