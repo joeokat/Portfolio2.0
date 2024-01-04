6dfdef33f74723af7f4072e0f3c4c4bad86e50dd
Joshua <joeokat@gmail.com>
2024-01-04 03:16:16 +0000

    commitAnyway

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -15,10 +15,11 @@ export default function Info() {
       </div>
 
       <div className="card">
+        <div className="b--ads">ads</div>
         <h2>Go from Zero to Booossu!</h2>
-        <p className="b--ads">Develop skills you can apply right away, with curated e-learning courses designed by <span>BYOI</span> experts. Learn at your own pace and get certified.</p>
+        <p>Develop skills you can apply right away, with curated e-learning courses designed by <span>BYOI</span> experts. <br /> Learn at your own pace and get certified.</p>
 
-        <a className="join--button" href="https://t.me/+oCeVAiPJM482NjI0" rel="noreferrer" target="_blank">Join us today<img src={message} alt="" className="icon" /></a>
+        <a className="join--button" href="https://t.me/+oCeVAiPJM482NjI0" rel="noreferrer" target="_blank">Join the Course Waitlist<img src={message} alt="" className="icon" /></a>
       </div>
 
       <div className="my--skills">
