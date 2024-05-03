e22a6f30be1619d9e1f662c7fab3b304465b1408
Joshua Ofosu <joeokat@gmail.com>
2024-05-03 21:46:39 +0000

    Portfolio--container rectified

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import React from "react";
 import "./App.css";
 import Profile from "./components/Profile.jsx";
 import { Services } from "./components/Services.jsx";
-// import Info from "./components/Info.jsx";
-// import { SpartanAds } from "./components/SpartanAds.jsx";
-// import { Portfolio } from "./components/Portfolio.jsx";
+// import Nav from "./components/Nav.jsx";
+import { Portfolio } from "./components/Portfolio.jsx";
 import Socials from "./components/Socials.jsx";
 
 export default function App() {
   return (
     <div className="App">
+      {/* <Nav /> */}
       <Profile />
       <Services />
-      {/* <Portfolio /> */}
-      {/* <Info /> */}
-      {/* <SpartanAds /> */}
+      <Portfolio />
       <Socials />
     </div>
   );
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,14 +6,14 @@ import odx from "../assets/odx.png";
 export const Portfolio = () => {
   return (
     <div className='portfolio--container'>
-        <h2>Recent Work</h2>
+      <h2 className='recent--work'>Recent Work</h2>
         
-            <div className="portfolio--row">
+        <div className="portfolio--row">
             <div className='portfolio--card'>
                 <img src={odx} alt="" />
                 <p className='title'>Spartan Booking App</p>
                 <p> Web Technologies: Svelte.js</p>
-                <a className='view--BTN' href="odx.vercel.app" target='_blank' > View Live Project
+                <a className='view--BTN' href="https://odx.vercel.app" target='_blank' rel='noreferrer'> View Live Project
                 <img src={web} alt="" />
                 </a>
             </div>
@@ -22,11 +22,11 @@ export const Portfolio = () => {
                 <img src={cv} alt="" />
                 <p className='title'>Portfolio Web App 2.0</p>
                 <p> Web Technologies: HTML + CSS</p>
-                <a className='view--BTN' href="#"> View Live Project
+                <a className='view--BTN' href="https://threads.net/fleetlabsgh" target='_blank' rel='noreferrer'> View Live Project
                 <img src={web} alt="" />
                 </a>
             </div>
-            </div>
+        </div>
     </div>
   )
 }
