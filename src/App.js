108654f4a67238ad768fd839d57992c992a5e6df
Joshua Ofosu <joeokat@gmail.com>
2024-05-08 15:52:17 +0000

    logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Nav from "./components/Nav.jsx";
 import Profile from "./components/Profile.jsx";
 import { Services } from "./components/Services.jsx";
-import Nav from "./components/Nav.jsx";
-
-import Socials from "./components/Socials.jsx";
+import { Partners } from "./components/Partners.jsx";
 import { SpartanAds } from "./components/SpartanAds.jsx";
+import Socials from "./components/Socials.jsx";
+
+
 
 export default function App() {
   return (
@@ -13,6 +15,7 @@ export default function App() {
       <Nav />
       <Profile />
       <Services />
+      <Partners />
       <SpartanAds />
       <Socials />
     </div>
--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import mrbeast from "../assets/logos/mrbeast.svg"
 
 export const Partners = () => {
   return (
     <div className='partner--container'>
         <h2>Partners</h2>
-        <div>---</div>
+        <div class="logos">
+            <div class="logos-slide">
+                
+                <img src={mrbeast} alt=''/>
+                <img src={mrbeast} alt=''/>
+            </div>
+        </div>
+
         <div className='cta-btn'>
-            <div className="see--mywork">See my work</div>
             <div className="pricing--plans">Pricing Plans</div>
+            <div className="see--mywork">See my work</div>
         </div>
     </div>
   )
 }
+
+<script>
+    let copy = document.querySelector(".logos-slide").cloneNode(true);
+    document.querySelector(".logos").appendChild(copy);
+</script>
