5f5066abb9ed43d17ddd547947793c71cfd1e424
Joshua Ofosu <joeokat@gmail.com>
2024-05-15 18:43:27 +0000

    PortfolioRefactor

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -30,3 +30,49 @@ export const Portfolio = () => {
     </div>
   )
 }
+
+
+// import React from 'react';
+// import web from "../assets/web.svg";
+// import cv from "../assets/cv.jpg";
+// import odx from "../assets/odx.png";
+
+// const Portfolio = () => {
+//   return (
+//     <div className='portfolio--container'>
+//       <h2 className='recent--work'>Recent Work</h2>
+        
+//       <div className="portfolio--row">
+//         <ProjectCard
+//           image={odx}
+//           title="Soccer Tips App"
+//           technologies="Web Technologies: Svelte.js"
+//           liveLink="https://odx.vercel.app"
+//         />
+        
+//         <ProjectCard
+//           image={cv}
+//           title="Portfolio Website 2.0"
+//           technologies="Web Technologies: HTML + CSS"
+//           liveLink="https://joshuaofosu.vercel.app/"
+//         />
+//       </div>
+//     </div>
+//   );
+// }
+
+// const ProjectCard = ({ image, title, technologies, liveLink }) => {
+//   return (
+//     <div className='portfolio--card'>
+//       <img src={image} alt={title} />
+//       <p className='title'>{title}</p>
+//       <p>{technologies}</p>
+//       <a className='view--BTN' href={liveLink} target='_blank' rel='noreferrer'>
+//         View Live Project
+//         <img src={web} alt="web icon" />
+//       </a>
+//     </div>
+//   );
+// }
+
+// export default Portfolio;
