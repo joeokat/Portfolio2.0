e82b8d697d22b1ecb28ddff798bad092714661e8
Joshua Ofosu <joeokat@gmail.com>
2024-05-16 00:32:18 +0000

    Update Partners.jsx

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
-//import linkedin from "../assets/logos/linkedin.svg"
-// import fleet from "../assets/logos/fleet.svg"
-//import figma from "../assets/logos/figma.svg"
-//import fiver from "../assets/logos/fiver.svg"
+import linkedin from "../assets/logos/linkedin.svg"
+import fleet from "../assets/logos/fleet.svg"
+import figma from "../assets/logos/figma.svg"
+import fiver from "../assets/logos/fiver.svg"
 
 export const Partners = () => {
   return (
     <div className='partner--container'>
         <h2>Partners</h2>
-        {/* <div class="logos">
+        <div class="logos">
             <div class="logos-slide">
                 <img src={fleet} alt=''/>
                 <img src={fiver} alt=''/>
                 <img src={figma} alt=''/>
                 <img src={linkedin} alt=''/>
+                <img src={fleet} alt=''/>
+                <img src={fiver} alt=''/>
+                <img src={figma} alt=''/>
+                <img src={linkedin} alt=''/>
             </div>
-        </div> */}
-        <p>⚠️ Loading...</p>
+        </div>
     </div>
   )
 }
