6d5c24c73c6dacd7dd5068d8baf07d4ad7b67a1a
Joshua Ofosu <joeokat@gmail.com>
2024-06-29 05:59:21 +0000

    Update SpartanAds.jsx

--- src/components/SpartanAds.jsx
+++ src/components/SpartanAds.jsx
@@ -6,10 +6,10 @@ export const SpartanAds = () => {
     
       <div className="card">
         <div className="b--ads">Spartan Ads</div>
-         <h2>BYOI E-Learning</h2>
-        <p>Access concise and beginner friendly mobile App Development courses with practical projects, professional certification, internships and mentorship support.</p>
+         <h2> Learn to Build & Publish Apps in 6-9 Weeks! </h2>
+        <p> Join our beginner-friendly courses to master mobile, desktop, and web development with HTML, CSS, & Reactjs. Enjoy hands-on projects, earn professional certifications, and get support with internships and mentorship! </p>
 
-        <a className="join--button" href="https://t.me/+oCeVAiPJM482NjI0" rel="noreferrer" target="_blank">Only ¢240/mo, Enroll Now</a>
+        <a className="join--button" href="https://t.me/+oCeVAiPJM482NjI0" rel="noreferrer" target="_blank">Only ¢240/mo, Enroll Now </a>
       </div>
     
   )
