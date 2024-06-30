a79b9c66310c63d84c727198a477c7ea07518a72
Joshua Ofosu <joeokat@gmail.com>
2024-06-30 14:28:09 +0000

    Update PricingPlans.jsx

--- src/components/PricingPlans.jsx
+++ src/components/PricingPlans.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-const PricingCard = ({ tag, tagClass, title, price, features }) => {
+
+const PricingCard = ({ tag, tagClass, title, service, price, features }) => {
+  const featureList = features.split(',').map((feature, index) => (
+    <p key={index}>{feature.trim()}</p>
+  ));
+
   return (
     <div className={`details--card ${tagClass}`}>
       <div className='tag'>{tag}</div>
       <h3>{title}</h3>
-      <p><strong>{price}</strong> /timeline</p>
+      <p><strong>{price}</strong> /gig</p>
+      <p>{service}</p>
       <hr />
-      <p> {features} <br /> </p>
+      <div>{featureList}</div>
       <button>Choose Plan</button>
     </div>
   );
@@ -17,30 +23,31 @@ export const PricingPlans = () => {
   return (
     <div className='pricing--container'>
       <h2>Select a Plan</h2>
+      <p>You are in good hands, get value for your order.</p>
       <div className='pricing--plans'>
         <PricingCard
           tag="Standard"
           tagClass="standard"
           title="Web Development"
-          service="Full Stack Web Development"
-          price="$500 - $1500"
-          features="Responsive Design, Backend Development, API Integration"
+          service="Jumpstart your business growth with a landing page or a full website"
+          price="$75"
+          features="Responsive Design, React.js Development, Simple Form Handling, Basic SEO Practices, API Integration"
         />
         <PricingCard
           tag="Popular"
           tagClass="popular"
           title="UI/UX Design"
-          service="UI/UX Design"
-          price="$300 - $1000"
-          features="Wireframes, Prototypes, User Research, User Testing"
+          service="I will design a visually stunning web application that targets your ideal clients"
+          price="$300"
+          features="User Research, Lo-Fi Wireframes, Hi-Fi Mockup Design, Interactive Prototypes, Micro Animation, User Testing"
         />
         <PricingCard
           tag="Best Value"
           tagClass="best-value"
           title="PWA Development"
           service="Progressive Web App Development"
-          price="$800 - $2000"
-          features="Offline Support, Fast Load Times, App-like Experience"
+          price="$116"
+          features="Source File, Fast Load Time, Mobile App for publishing, App-like Experience, Add to Home Screen Functionality"
         />
       </div>
     </div>
