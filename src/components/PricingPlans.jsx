d64c86bf2ae05e30787894232376d53f672c4380
Joshua Ofosu <joeokat@gmail.com>
2024-06-30 13:11:14 +0000

    cards

--- src/components/PricingPlans.jsx
+++ src/components/PricingPlans.jsx
@@ -1,21 +1,53 @@
-import React from 'react'
+import React from 'react';
 
 export const PricingPlans = () => {
   return (
-    <>
-      <div className='pricing--container'>
-      <h2 className='pending'>Select a plan</h2>
-        <div>
-        <details>
-          <summary>Freelance</summary>
-          <h2 className='pending'>You can check out Gig pricing on Fiverr for remote services only</h2>
-        </details>
-        <details>
-          <summary>Professional</summary>
-          <h2 className='pending'>You can check out Gig pricing on Fiverr for remote services only</h2>
-        </details>
+    <div className='pricing--container'>
+      <h2>Select a Plan</h2>
+      <div className='pricing--plans'>
+        <div className='details--card standard'>
+          <div className='tag'>Standard</div>
+          <h3>Web Development</h3>
+          <hr/>
+          <p>
+            <strong>Service:</strong> Full Stack Web Development<br/>
+            <strong>Price:</strong> $500 - $1500<br/>
+          </p>
+          <hr/>
+          <p>
+            <strong>Features:</strong> Responsive Design, Backend Development, API Integration
+          </p>
+          <button>Choose Plan</button>
+        </div>
+        <div className='details--card popular'>
+          <div className='tag'>Popular</div>
+          <h3>UI/UX Design</h3>
+          <hr/>
+          <p>
+            <strong>Service:</strong> UI/UX Design<br/>
+            <strong>Price:</strong> $300 - $1000<br/>
+          </p>
+          <hr/>
+          <p>
+            <strong>Features:</strong> Wireframes, Prototypes, User Research, User Testing
+          </p>
+          <button>Choose Plan</button>
+        </div>
+        <div className='details--card best-value'>
+          <div className='tag'>Best Value</div>
+          <h3>PWA Development</h3>
+          <hr/>
+          <p>
+            <strong>Service:</strong> Progressive Web App Development<br/>
+            <strong>Price:</strong> $800 - $2000<br/>
+          </p>
+          <hr/>
+          <p>
+            <strong>Features:</strong> Offline Support, Fast Load Times, App-like Experience
+          </p>
+          <button>Choose Plan</button>
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
