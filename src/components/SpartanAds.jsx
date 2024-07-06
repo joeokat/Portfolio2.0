ed2ecd131c3d50829536774513a9683b24a6f5c0
Joshua Ofosu <joeokat@gmail.com>
2024-07-06 20:49:51 +0000

    Ads

--- src/components/SpartanAds.jsx
+++ src/components/SpartanAds.jsx
@@ -9,10 +9,16 @@ export const SpartanAds = () => {
          <h2> Learn to Build Web Apps </h2>
         <p> 
           Digital products are very easy to build. Most people just don’t have the right ideas. 
-          Get my FULL GUIDE teaching you how to make ¢2,500/m+ in a few weeks.
+          Get my FULL GUIDE teaching you how to make ¢2,500+ in a few months.
         </p>
 
-        <a className="join--button" href="https://t.me/+oCeVAiPJM482NjI0" rel="noreferrer" target="_blank"> Get it for only ¢150 </a>
+        <a 
+        className="join--button" 
+        href="https://t.me/+oCeVAiPJM482NjI0" 
+        rel="noreferrer" 
+        target="_blank"> 
+          Get it for only ¢150 
+        </a>
       </div>
     </div>
   )
