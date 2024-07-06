ccd077e2ac0e1cfcc8b6789cb84d6af1dea968a5
Joshua Ofosu <joeokat@gmail.com>
2024-07-06 15:34:02 +0000

    12px

--- src/components/SpartanAds.jsx
+++ src/components/SpartanAds.jsx
@@ -6,11 +6,11 @@ export const SpartanAds = () => {
     <div className="card--container">
       <div className="card">
         <div className="b--ads">Spartan Ads</div>
-         <h2> Learn to Build & Publish Web Apps </h2>
+         <h2> Learn to Build Web Apps </h2>
         <p> 
-          Three of my students made ¢2,500 each this month by developing a simple web application
-           and the truth is, digital products are very easy to build. It's just most people just don’t have the right ideas. <br />
-          So I made a FULL GUIDE teaching you how to make ¢2,000/m+ in my course.
+          Three of my students made ¢2,500 each last month by publishing a simple web application
+           and the truth is, digital products are very easy to build. Most people just don’t have the right ideas. <br />
+          I made a FULL GUIDE teaching you how to make ¢2,000/m+ in my course.
         </p>
 
         <a className="join--button" href="https://t.me/+oCeVAiPJM482NjI0" rel="noreferrer" target="_blank"> Get it for only $7 </a>
